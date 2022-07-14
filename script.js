4f5b978214197fe7b73c282af6b6b0bc65172a42
Johnathan <jocuog@gmail.com>
2022-07-13 22:08:21 -0600

    emojis to match expression

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const video = document.getElementById('video');
-
+document.getElementById('emoji-express');
 Promise.all([
     faceapi.nets.tinyFaceDetector.loadFromUri('/models/models'),
     faceapi.nets.faceLandmark68Net.loadFromUri('/models/models'),
@@ -16,6 +16,22 @@ function startVideo() {
 }
 // startVideo()
 
+// video.addEventListener('play', () =>{
+//     const canvas = faceapi.createCanvasFromMedia(video)
+//     document.body.append(canvas)
+//     const displaySize = {width: video.width, height: video.height}
+//     faceapi.matchDimensions(canvas, displaySize)
+//     setInterval(async () => {
+//         const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+//         const resizedDetections = faceapi.resizeResults(detections, displaySize)
+//         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+//         faceapi.draw.drawDetections(canvas, resizedDetections)
+//         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+//         faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+//         console.log(detections)
+//     }, 100)
+// })
+
 video.addEventListener('play', () =>{
     const canvas = faceapi.createCanvasFromMedia(video)
     document.body.append(canvas)
@@ -28,5 +44,39 @@ video.addEventListener('play', () =>{
         faceapi.draw.drawDetections(canvas, resizedDetections)
         faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
         faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+        const detectionsExpressions = detections[0].expressions
+        // console.log(detectionsExpressions)
+        const maxExpressions = Object.entries(detectionsExpressions).sort((x,y) => y[1] - x[1]) [0]
+        console.log(maxExpressions)
+        switch (maxExpressions[0]) {
+            case 'neutral': 
+                console.log('neutral expression');
+                document.getElementById('emoji-express').innerHTML = '😐'
+            break;
+            case 'angry': 
+                console.log('angry expression');
+                document.getElementById('emoji-express').innerHTML = '😠'
+            break;
+            case 'disgusted': 
+                console.log('disgusted expression');
+                document.getElementById('emoji-express').innerHTML = '🤢'
+            break;
+            case 'fearful':
+                console.log('fearful expression');
+                document.getElementById('emoji-express').innerHTML = '😱'
+            break;
+            case 'happy': 
+                console.log('happy expression');
+                document.getElementById('emoji-express').innerHTML = '😁'
+            break;
+            case 'sad': 
+                console.log('sad expression');
+                document.getElementById('emoji-express').innerHTML = '😢'
+            break;
+            case 'surprised': 
+                console.log('surprised expression');
+                document.getElementById('emoji-express').innerHTML = '😲'
+            break;
+        }
     }, 100)
 })
